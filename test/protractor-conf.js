a047bcc1777493c4bf19633e6f3bedd985a630a9
Ming He <meticulous.dft@gmail.com>
2014-08-15 15:38:40 -0700

    conf for e2e test

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -1,4 +1,8 @@
+var HtmlReporter = require('protractor-html-screenshot-reporter');
+
 exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
   allScriptsTimeout: 11000,
 
   specs: [
@@ -6,10 +10,26 @@ exports.config = {
   ],
 
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'phantomjs',
+
+  /* 
+   * Can be used to specify the phantomjs binary path.
+   * This can generally be ommitted if you installed phantomjs globally.
+   */
+    //'phantomjs.binary.path':'./node_modules/phantomjs/bin/phantomjs',
+
+
+  /*
+   * Command line arugments to pass to phantomjs. 
+   * Can be ommitted if no arguments need to be passed. 
+   * Acceptable cli arugments: https://github.com/ariya/phantomjs/wiki/API-Reference#wiki-command-line-options
+   */
+    //'phantomjs.cli.args':['--ignore-ssl-errors=true',  '--web-security=false', '--logfile=/root/repo/test_out/e2e.log', '--loglevel=DEBUG']
+    'phantomjs.cli.args':['--ignore-ssl-errors=true',  '--web-security=false', '--webdriver-logfile=/root/repo/test_out/e2e.log', '--webdriver-loglevel=DEBUG']
+
   },
 
-  chromeOnly: true,
+  //chromeOnly: false,
 
   baseUrl: 'http://localhost:8000/',
 
@@ -17,5 +37,13 @@ exports.config = {
 
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
+  },
+
+  onPrepare: function() {
+    // Add a screenshot reporter and store screenshots to `/tmp/screnshots`:
+    jasmine.getEnv().addReporter(new HtmlReporter({
+      baseDirectory: '/root/repo/test_e2e'
+    }));
   }
+
 };
