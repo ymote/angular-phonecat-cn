13c0539ac6583b1af044c38ee45633b7130d08d0
Ming He <meticulous.dft@gmail.com>
2014-10-30 00:11:28 -0700

    remove unnecessary files

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,7 @@ var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
   $http.get('phones/phones.json').success(function(data) {
+    //修改以下代码使得只显示手机列表的前五个
     $scope.phones = data;
   });
 
