700ef0d8b57b50900f529863b74ac966351db851
Ming He <meticulous.dft@gmail.com>
2014-11-11 23:08:52 -0800

    add check answer

---
+++ test/e2e/scenarios.js
@@ -0,0 +1,29 @@
+'use strict';
+
+describe('ng-repeat filter', function() {
+
+  describe('index page', function() {
+
+    beforeEach(function() {
+      browser.get('index.html');
+    });
+
+    it('should show numbers 0 to 7 in a table', function() {
+      element.all(by.css('table tr')).then(function(trs){
+        expect(trs.length).toBe(9);
+        if(trs.length === 9){
+          expect(trs[1].element(by.css('td')).getText()).toBe('0');
+          expect(trs[2].element(by.css('td')).getText()).toBe('1');
+          expect(trs[3].element(by.css('td')).getText()).toBe('2');
+          expect(trs[4].element(by.css('td')).getText()).toBe('3');
+          expect(trs[5].element(by.css('td')).getText()).toBe('4');
+          expect(trs[6].element(by.css('td')).getText()).toBe('5');
+          expect(trs[7].element(by.css('td')).getText()).toBe('6');
+          expect(trs[8].element(by.css('td')).getText()).toBe('7');
+        }
+      });
+    });
+
+  });
+
+});
