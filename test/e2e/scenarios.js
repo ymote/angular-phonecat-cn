712b7fe244772088f168c282498ca557c4148b8f
Meticulous DFT <meticulous.dft@gmail.com>
2014-11-22 23:51:37 -0800

    Update scenarios.js

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -24,7 +24,7 @@ describe('PhoneCat App', function() {
       query.clear();
       query.sendKeys('motorola');
       //phoneList这时应该有几个phone?
-      expect(phoneList.count()).toBe(2);
+      expect(phoneList.count()).toBe(0);
     });
 
 
