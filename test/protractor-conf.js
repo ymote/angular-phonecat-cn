3639a5fb034c16fec7ba3671e1f1f3a0e1f3864f
live4code <live4code@gmail.com>
2015-03-10 23:00:13 +0000

    commit exercise content by Live4Code

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -42,5 +42,12 @@ exports.config = {
 
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
+  },
+  
+  onPrepare: function() {
+    // Add a screenshot reporter and store screenshots to `/tmp/screnshots`:
+    jasmine.getEnv().addReporter(new HtmlReporter({
+      baseDirectory: 'test_out'
+    }));
   }
 };
